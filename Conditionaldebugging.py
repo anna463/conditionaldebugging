a9f62d881f42ae2a4b85f8551c8004cb46e91df4
anna463 <57601829+anna463@users.noreply.github.com>
2020-02-02 16:27:04 -0600

    Add files via upload

---
+++ Conditionaldebugging.py
@@ -0,0 +1,59 @@
+'''
+Created on Feb 2, 2020
+
+@author: ITAUser
+'''
+If the problem is: tire, it says: Replace tires.
+
+If the problem is: headlight, it says: Fill headlight fluid.
+
+If the problem is: door, it says: replace door.
+
+If the problem is: gas, it says: Fill gas tank.
+
+If the problem is: window, it says: Replace window.
+
+If the problem is: wipers, it says: Replace windshield wipers.
+
+If the problem is: battery, it says: Replace battery.
+
+If the problem is: exhaust, it says: Replace exhaust system.
+
+If the problem is: transmission, it says: Throw out car.
+
+If the problem is: anything else, it says: Your car is fine.
+'''
+def fixMyCar(problem):
+    if(problem == "tire"):
+    print("Replace tires")
+    elif (problem == "headlight"):
+        print("Fill headlight fluid")
+    if(problem == "door"):
+        print("replace door")
+    elif (problem == "gas")
+        print("Fill gas tank")
+    elif(problem == "window"):
+        print("Replace window")
+    elif(problem == "wipers"):
+        print("Replace windshield wipers")
+    elif(problem == "battery"):
+        print("Replace battery")
+    else(problem == "exhaust"):
+        print("Replace exhaust system")
+    if(problem == "transmission"):
+        print("Throw out car")
+    elif:
+        print("Your car is fine.")
+    
+    return 
+
+fixMyCar("tire")
+fixMyCar("headlight")
+fixMyCar("door")
+fixMyCar("gas")
+fixMyCar("window")
+fixMyCar("wipers")
+fixMyCar("battery")
+fixMyCar("exhaust")
+fixMyCar("transmission")
+fixMyCar("Nothing")
